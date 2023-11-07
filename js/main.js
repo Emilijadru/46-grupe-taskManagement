90d5e0f7d43ccb918c181b3d0f23f836d8230655
Emilija <145319859+Emilijadru@users.noreply.github.com>
2023-11-07 15:17:37 +0200

    Todo render

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-import { minus, multi, sum } from './sum.js';
+import { Todo } from "./Todo.js";
 
-console.log('labas');
+const kanban = new Todo('#kanban', ['Backlock', 'Todo', 'In progress', 'Done']);
 
-const n1 = 7;
-const n2 = 5;
-
-
-const n12 = sum(n1, n2);
-const n12minus = minus(n1, n2);
-const n12multi = multi(n1, n2);
-
-console.log(n12);
-console.log(n12minus);
-console.log(n12multi);
